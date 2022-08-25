e114e82a2171efd7eaafb6cf4756e7bee92aee10
Evgeny Preobrazhenskii <jinprett@gmail.com>
2022-08-25 14:56:56 +0300

    задача с гирями

---
+++ Example_06_Base/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 1, b = 8, c = 6, d = 2, e = 4;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("max = " + max);
