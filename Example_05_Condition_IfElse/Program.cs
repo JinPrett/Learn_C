01b8ee49f4f7a6a12433a944b4533d81a41bad88
Evgeny Preobrazhenskii <jinprett@gmail.com>
2022-08-25 14:30:35 +0300

    Проверяем работу IfElse и ToLower

---
+++ Example_05_Condition_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Добрый день! Введите своё имя, пожалуйста: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "наташа")
+{
+    Console.WriteLine("Ура, это же Моя Любимка!!!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username);
+}
