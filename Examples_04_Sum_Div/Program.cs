43f96e6f279b8e140cf9df9a73760701f1b005a3
Evgeny Preobrazhenskii <jinprett@gmail.com>
2022-08-25 12:13:37 +0300

    Примеры с суммой и делением (с остатком)

---
+++ Examples_04_Sum_Div/Program.cs
@@ -0,0 +1,8 @@
+﻿double numberA = 1;
+double numberB = 5;
+double resultSum = numberA + numberB;
+double resultDiv = numberA / numberB;
+Console.Write("Сумма чисел ");
+Console.WriteLine(resultSum);
+Console.Write("Частное чисел ");
+Console.WriteLine(resultDiv);
